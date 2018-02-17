d9edf42f327692d564cf5e9b132fa6ea2063eb41
sureshnswamy <sureshnswamy@gmail.com>
2018-02-17 22:41:44 +1300

    add karma test

--- karma.conf.js
+++ karma.conf.js
@@ -1,10 +1,15 @@
+const webpackConfig = require('./webpack.config.babel')({env: 'test'})
+const fileGlob = 'src/**/*.test.js'
+const preprocessors ={
+    [fileGlob]: ['webpack'],
+}
 module.exports = config => {
   config.set({
     basePath: '',
     frameworks: ['mocha', 'chai'],
-    files: [
-      'src/**/*.test.js',
-    ],
+    files: [fileGlob],
+    preprocessors,
+    webpack: webpackConfig,
     reporters: ['progress'],
     port: 9876,
     colors: true,
--- src/controller.test.js
+++ src/controller.test.js
@@ -1,8 +1,8 @@
+const Controller = require('./controller')
+
 describe('test', () => {
   it('works', () => {
-    expect(true).to.be.true
+    expect(Controller).to.exist
   })
-  it('works again', () => {
-    expect('hi').to.equal('hi')
-  })
+  
 })
